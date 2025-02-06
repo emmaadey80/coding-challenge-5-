1de2d5faea853dd9f4e0c1374c36a7e1a1304950
Emma Adey <eadey@usf.edu>
2025-02-05 19:04:03 -0500

    Task 2- Order Details

--- cc_5.js
+++ cc_5.js
@@ -14,3 +14,23 @@ email: "john.doe@email.com"
 console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`);
+
+//Task 2- Object Methods
+console.log("\nTask 2- Object Methods");
+
+const order = { // Decalring object "order" with properties 
+    orderID: 12345,
+    totalAmount: 150,
+    status: "Processing",
+
+    // Display the order details (orderID, totalAmount, status)
+    displayOrder: function() {
+      console.log(`Order ID: ${this.orderID}`);
+      console.log(`Total Amount: $${this.totalAmount}`);
+      console.log(`Status: ${this.status}`);
+    }
+  };
+  
+  // Call the displayOrder method
+  order.displayOrder();
+
