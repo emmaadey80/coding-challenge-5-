dd98f790a9d02847f791b7da659053708c713e2c
Emma Adey <eadey@usf.edu>
2025-02-05 19:00:13 -0500

    Task 1 - Customer Profile

--- cc_5.js
+++ cc_5.js
@@ -1,2 +1,16 @@
 // Inital Commit - Objects, Arrays, and Functions Challenge Setup
 
+
+// Task 1- Object Properties
+console.log("Task 1- Object Properties");
+
+let customer = {  // Declaring a customer object with properties
+name: "John Doe", 
+age: 35, 
+email: "john.doe@email.com"
+};
+
+// Logging each property (Name, Age, Email)
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
